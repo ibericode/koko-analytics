6626067ebe6b3c4e167996f167f787bdd1a2e319
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-04 21:17:34 +0100

    add sample root file for speeding up tracking requests even more

---
+++ koko-analytics-collect.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Assumes a default WordPress installation, needs to be placed in the root directory.
+ */
+
+define('KOKO_ANALYTICS_BUFFER_FILE', __DIR__ . '/wp-content/uploads/pageviews.php');
+
+require __DIR__ . '/wp-content/plugins/koko-analytics/src/functions.php';
+
+KokoAnalytics\collect_request();
--- src/functions.php
+++ src/functions.php
@@ -9,36 +9,46 @@ function maybe_collect_request() {
         return;
     }
 
-    $unique_visitor = (int) $_GET['nv'];
-    $unique_pageview = (int) $_GET['up'];
-    $post_id = (int) $_GET['p'];
-    $referrer = isset($_GET['r']) ? trim($_GET['r']) : '';
+    collect_request();
+}
 
-    collect_in_file($post_id, $unique_visitor, $unique_pageview, $referrer);
+function collect_request() {
+	$unique_visitor = (int) $_GET['nv'];
+	$unique_pageview = (int) $_GET['up'];
+	$post_id = (int) $_GET['p'];
+	$referrer = isset($_GET['r']) ? trim($_GET['r']) : '';
 
-    // set OK headers & prevent caching
-    status_header(200);
-    header('Content-Type: image/gif');
-    header('X-Content-Type-Options: nosniff');
-    header('Expires: Wed, 11 Jan 1984 05:00:00 GMT');
-    header('Cache-Control: no-cache, must-revalidate, max-age=0');
-    header_remove('Last-Modified');
-    send_origin_headers();
+	collect_in_file($post_id, $unique_visitor, $unique_pageview, $referrer);
 
-    // indicate that we are not tracking user specifically, see https://www.w3.org/TR/tracking-dnt/
-    header('Tk: N');
+	// set OK headers & prevent caching
+	header($_SERVER["SERVER_PROTOCOL"] . ' 200 OK');
+	header('Content-Type: image/gif');
+	header('X-Content-Type-Options: nosniff');
+	header('Expires: Wed, 11 Jan 1984 05:00:00 GMT');
+	header('Cache-Control: no-cache, must-revalidate, max-age=0');
+	header_remove('Last-Modified');
 
-    // 1px transparent GIF, needs to be an actual image to make sure browser fires the onload event
-    echo base64_decode('R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
-    exit;
+	// TODO: Determine whether we need to set origin headers and if we can do this without loading WordPress
+	// send_origin_headers();
+
+	// indicate that we are not tracking user specifically, see https://www.w3.org/TR/tracking-dnt/
+	header('Tk: N');
+
+	// 1px transparent GIF, needs to be an actual image to make sure browser fires the onload event
+	echo base64_decode('R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
+	exit;
 }
 
 function collect_in_file($post_id, $is_new_visitor, $is_unique_pageview, $referrer = '')
 {
-    $line = join(',', array($post_id, $is_new_visitor, $is_unique_pageview, $referrer));
-    $uploads = wp_get_upload_dir();
-    $filename = $uploads['basedir'] . '/pageviews.php';
-    $content = '';
+	if (defined('KOKO_ANALYTICS_BUFFER_FILE')) {
+		$filename = KOKO_ANALYTICS_BUFFER_FILE;
+	} else {
+		$uploads = wp_get_upload_dir();
+		$filename = $uploads['basedir'] . '/pageviews.php';
+	}
+
+	$content = '';
 
     // if file does not yet exist, add PHP header to prevent direct file access
     if (!file_exists($filename)) {
@@ -46,6 +56,7 @@ function collect_in_file($post_id, $is_new_visitor, $is_unique_pageview, $referr
     }
 
     // append data to file
-    $content .= $line . PHP_EOL;
+	$line = join(',', array($post_id, $is_new_visitor, $is_unique_pageview, $referrer));
+	$content .= $line . PHP_EOL;
     return file_put_contents($filename, $content, FILE_APPEND);
 }
