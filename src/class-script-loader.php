ca940258725e0699c1ebdd07801bfb116407ea4a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-06 15:28:22 +0100

    load script asynchronously

--- src/class-script-loader.php
+++ src/class-script-loader.php
@@ -8,10 +8,10 @@ class ScriptLoader
 {
     public function init()
     {
-        add_action('wp_head', array($this, 'maybe_enqueue_script'));
+        add_action('wp_footer', array($this, 'maybe_print_script_tag'), 90);
     }
 
-    public function maybe_enqueue_script() {
+    public function maybe_print_script_tag() {
         $settings = get_settings();
         $user = wp_get_current_user();
 
@@ -23,11 +23,13 @@ class ScriptLoader
         // TODO: Handle "term" requests so we track both terms and post types.
         $post_id = is_singular() ? (int) get_queried_object_id() : 0;
         $use_custom_endpoint = (defined('KOKO_ANALYTICS_USE_CUSTOM_ENDPOINT') && KOKO_ANALYTICS_USE_CUSTOM_ENDPOINT) || file_exists(ABSPATH . '/koko-analytics-collect.php');
-        wp_enqueue_script('koko-analytics-script', plugins_url('assets/dist/js/script.js', KOKO_ANALYTICS_PLUGIN_FILE), array(), KOKO_ANALYTICS_VERSION, true);
-        wp_localize_script('koko-analytics-script', 'koko_analytics', array(
-            'post_id' => $post_id,
-            'tracker_url' => $use_custom_endpoint ? home_url('/koko-analytics-collect.php') : admin_url('admin-ajax.php'),
-        ));
+        $script_data = array(
+			'post_id' => $post_id,
+			'tracker_url' => $use_custom_endpoint ? home_url('/koko-analytics-collect.php') : admin_url('admin-ajax.php'),
+		);
+
+		echo sprintf('<script type="text/javascript">/* <![CDATA[ */var koko_analytics = %s;/* ]]> */</script>', json_encode($script_data));
+		echo sprintf('<script type="text/javascript" src="%s?ver=%s" async="async"></script>', plugins_url('assets/dist/js/script.js', KOKO_ANALYTICS_PLUGIN_FILE), KOKO_ANALYTICS_VERSION);
     }
 
     public function user_has_roles(WP_User $user, array $roles) {
