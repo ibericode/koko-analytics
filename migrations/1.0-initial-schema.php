1622fd1615cfd730441b6cedc92d36456071d273
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-13 08:32:50 +0100

    add views/ and migrations/ dir to codesniffer config

--- migrations/1.0-initial-schema.php
+++ migrations/1.0-initial-schema.php
@@ -1,37 +1,45 @@
 <?php
 
-defined('ABSPATH') or exit;
+defined( 'ABSPATH' ) or exit;
 
 global $wpdb;
 
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_site_stats");
-$wpdb->query("CREATE TABLE {$wpdb->prefix}koko_analytics_site_stats (
-   date DATE PRIMARY KEY NOT NULL,
-   visitors INTEGER UNSIGNED NOT NULL,
-   pageviews INTEGER UNSIGNED NOT NULL
-) ENGINE=INNODB CHARACTER SET=ascii");
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_site_stats" );
+$wpdb->query(
+	"CREATE TABLE {$wpdb->prefix}koko_analytics_site_stats (
+		   date DATE PRIMARY KEY NOT NULL,
+		   visitors INTEGER UNSIGNED NOT NULL,
+		   pageviews INTEGER UNSIGNED NOT NULL
+	) ENGINE=INNODB CHARACTER SET=ascii"
+);
 
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_post_stats");
-$wpdb->query("CREATE TABLE {$wpdb->prefix}koko_analytics_post_stats (
-   date DATE NOT NULL,
-   id BIGINT(20) UNSIGNED NOT NULL,
-   visitors INTEGER UNSIGNED NOT NULL,
-   pageviews INTEGER UNSIGNED NOT NULL,
-   PRIMARY KEY (date, id)
-) ENGINE=INNODB CHARACTER SET=ascii");
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_post_stats" );
+$wpdb->query(
+	"CREATE TABLE {$wpdb->prefix}koko_analytics_post_stats (
+	   date DATE NOT NULL,
+	   id BIGINT(20) UNSIGNED NOT NULL,
+	   visitors INTEGER UNSIGNED NOT NULL,
+	   pageviews INTEGER UNSIGNED NOT NULL,
+	   PRIMARY KEY (date, id)
+	) ENGINE=INNODB CHARACTER SET=ascii"
+);
 
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_referrer_stats");
-$wpdb->query("CREATE TABLE {$wpdb->prefix}koko_analytics_referrer_stats (
-   date DATE NOT NULL,
-   id BIGINT(20) UNSIGNED NOT NULL,
-   visitors INTEGER UNSIGNED NOT NULL,
-   pageviews INTEGER UNSIGNED NOT NULL,
-   PRIMARY KEY (date, id)
-) ENGINE=INNODB CHARACTER SET=ascii");
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_referrer_stats" );
+$wpdb->query(
+	"CREATE TABLE {$wpdb->prefix}koko_analytics_referrer_stats (
+	   date DATE NOT NULL,
+	   id BIGINT(20) UNSIGNED NOT NULL,
+	   visitors INTEGER UNSIGNED NOT NULL,
+	   pageviews INTEGER UNSIGNED NOT NULL,
+	   PRIMARY KEY (date, id)
+	) ENGINE=INNODB CHARACTER SET=ascii"
+);
 
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_referrer_urls");
-$wpdb->query("CREATE TABLE {$wpdb->prefix}koko_analytics_referrer_urls (
-   id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   url VARCHAR(255) NOT NULL,
-   UNIQUE INDEX (url)
-) ENGINE=INNODB CHARACTER SET=ascii");
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_referrer_urls" );
+$wpdb->query(
+	"CREATE TABLE {$wpdb->prefix}koko_analytics_referrer_urls (
+	   id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	   url VARCHAR(255) NOT NULL,
+	   UNIQUE INDEX (url)
+	) ENGINE=INNODB CHARACTER SET=ascii"
+);
--- views/admin-page.php
+++ views/admin-page.php
@@ -1,40 +1,44 @@
-<?php defined('ABSPATH') or exit; ?>
+<?php defined( 'ABSPATH' ) or exit; ?>
 <div class="wrap" id="koko-analytics-admin">
-    <noscript>
-        <?php echo __( 'Please enable JavaScript for this page to work.', 'koko-analytics'); ?>
-    </noscript>
+	<noscript>
+		<?php echo __( 'Please enable JavaScript for this page to work.', 'koko-analytics' ); ?>
+	</noscript>
 
-    <div id="koko-analytics-mount"></div>
+	<div id="koko-analytics-mount"></div>
 </div>
 <script>
-    <?php echo 'var koko_analytics = ' . json_encode(array(
-            'root' => rest_url(),
-            'nonce' => wp_create_nonce('wp_rest'),
-			'i18n' => array(
-				'Date range' => __('Date range', 'koko-analytics'),
-				'Exclude pageviews from these user roles' => __('Exclude pageviews from these user roles', 'koko-analytics'),
-				'Last week' => __('Last week', 'koko-analytics'),
-				'Last month' => __('Last month', 'koko-analytics'),
-				'Last year' => __('Last year', 'koko-analytics'),
-				'Pages' => __('Pages', 'koko-analytics'),
-				'Pageviews' => __('Pageviews', 'koko-analytics'),
-				'Referrers' => __('Referrers', 'koko-analytics'),
-				'Save Changes' => __('Save Changes', 'koko-analytics'),
-				'Saving - please wait' => __('Saving - please wait', 'koko-analytics'),
-                'Saved!' => __('Saved!', 'koko-analytics'),
-				'Settings' => __('Settings', 'koko-analytics'),
-                'Stats' => __('Stats', 'koko-analytics'),
-				'There\'s nothing here, yet!' => __('There\'s nothing here, yet!', 'koko-analytics'),
-				'This week' => __('This week', 'koko-analytics'),
-				'This month' => __('This month', 'koko-analytics'),
-				'This year' => __('This year', 'koko-analytics'),
-				'Total visitors' => __('Total visitors', 'koko-analytics'),
-				'Total pageviews' => __('Total pageviews', 'koko-analytics'),
-				'Visitors' => __('Visitors', 'koko-analytics'),
+	<?php
+	echo 'var koko_analytics = ' . json_encode(
+		array(
+			'root'          => rest_url(),
+			'nonce'         => wp_create_nonce( 'wp_rest' ),
+			'i18n'          => array(
+				'Date range'                              => __( 'Date range', 'koko-analytics' ),
+				'Exclude pageviews from these user roles' => __( 'Exclude pageviews from these user roles', 'koko-analytics' ),
+				'Last week'                               => __( 'Last week', 'koko-analytics' ),
+				'Last month'                              => __( 'Last month', 'koko-analytics' ),
+				'Last year'                               => __( 'Last year', 'koko-analytics' ),
+				'Pages'                                   => __( 'Pages', 'koko-analytics' ),
+				'Pageviews'                               => __( 'Pageviews', 'koko-analytics' ),
+				'Referrers'                               => __( 'Referrers', 'koko-analytics' ),
+				'Save Changes'                            => __( 'Save Changes', 'koko-analytics' ),
+				'Saving - please wait'                    => __( 'Saving - please wait', 'koko-analytics' ),
+				'Saved!'                                  => __( 'Saved!', 'koko-analytics' ),
+				'Settings'                                => __( 'Settings', 'koko-analytics' ),
+				'Stats'                                   => __( 'Stats', 'koko-analytics' ),
+				'There\'s nothing here, yet!'             => __( 'There\'s nothing here, yet!', 'koko-analytics' ),
+				'This week'                               => __( 'This week', 'koko-analytics' ),
+				'This month'                              => __( 'This month', 'koko-analytics' ),
+				'This year'                               => __( 'This year', 'koko-analytics' ),
+				'Total visitors'                          => __( 'Total visitors', 'koko-analytics' ),
+				'Total pageviews'                         => __( 'Total pageviews', 'koko-analytics' ),
+				'Visitors'                                => __( 'Visitors', 'koko-analytics' ),
 			),
-            'start_of_week' => $start_of_week,
-            'user_roles' => $user_roles,
-            'settings' => $settings,
-        )); ?>
+			'start_of_week' => $start_of_week,
+			'user_roles'    => $user_roles,
+			'settings'      => $settings,
+		)
+	);
+	?>
 </script>
-<script src="<?php echo sprintf('%s?ver=%s', plugins_url('assets/dist/js/admin.js', KOKO_ANALYTICS_PLUGIN_FILE), KOKO_ANALYTICS_VERSION); ?>"></script>
+<script src="<?php echo sprintf( '%s?ver=%s', plugins_url( 'assets/dist/js/admin.js', KOKO_ANALYTICS_PLUGIN_FILE ), KOKO_ANALYTICS_VERSION ); ?>"></script>
