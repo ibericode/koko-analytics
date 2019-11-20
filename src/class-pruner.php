5b7d61d84f9163fb0e2157b1929b4d139c48e17b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-20 15:24:10 +0100

    delete unused referrer url's while pruning

--- src/class-pruner.php
+++ src/class-pruner.php
@@ -28,11 +28,14 @@ class Pruner {
 		}
 
 		$date = gmdate( 'Y-m-d', strtotime( "-{$settings['prune_data_after_months']} months" ) );
+
+		// delete stats older than date above
 		$wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}koko_analytics_site_stats WHERE date < %s", $date ) );
 		$wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}koko_analytics_post_stats WHERE date < %s", $date ) );
 		$wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}koko_analytics_referrer_stats WHERE date < %s", $date ) );
 
-		// TODO: Delete unused referrer URL's
+		// delete unused referrer URL's
+		$wpdb->query( "DELETE FROM {$wpdb->prefix}koko_analytics_referrer_urls WHERE id NOT IN ( SELECT id FROM {$wpdb->prefix}koko_analytics_referrer_stats )" );
 	}
 
 }
