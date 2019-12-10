cb28edb4a7ac997f5a53bedfdd08eed79f68dbc4
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-12-10 12:25:14 +0100

    fix variable type for postId so uniquie pageviews are properly detected

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -5,12 +5,13 @@
  */
 
 const trackerUrl = window.koko_analytics.tracker_url;
-const postId = parseInt(window.koko_analytics.post_id);
+const postId = parseInt(window.koko_analytics.post_id).toString();
 const useCookie = parseInt(window.koko_analytics.use_cookie);
 
 function stringifyObject(obj) {
 	return Object.keys(obj).map(function(k) {
-		return window.encodeURIComponent(k) + '=' + window.encodeURIComponent(obj[k]);
+		const v = obj[k];
+		return window.encodeURIComponent(k) + '=' + window.encodeURIComponent(v);
 	}).join('&');
 }
 
@@ -70,25 +71,21 @@ function trackPageview() {
 	}
 
 	const cookie = getCookie('_koko_analytics_pages_viewed');
-	const isNewVisitor = cookie.length === 0;
-	const pagesViewed = cookie.split(',').filter(function(id) { return id !== ''; });
-	const isUniquePageview = pagesViewed.indexOf(postId) === -1;
-	const d = {
-		p:  postId,
-		nv: isNewVisitor ? 1 : 0,
-		up: isUniquePageview ? 1 : 0,
-	};
+	let isNewVisitor = cookie.length === 0;
+	let pagesViewed = cookie.split(',').filter(function(id) { return id !== ''; });
+	let isUniquePageview = pagesViewed.indexOf(postId) === -1;
+	let referrer = '';
 
 	// add referrer if not from same-site & try to detect returning visitors from referrer URL
 	if (typeof(document.referrer) === "string" && document.referrer !== '') {
 		if (document.referrer.indexOf(window.location.origin) === 0) {
-			d.nv = 0; // referred by same-site, so not a new visitor
+			isNewVisitor = false; // referred by same-site, so not a new visitor
 
 			if (document.referrer === window.location.href) {
-				d.up = 0; // referred by same-url, so not a unique pageview
+				isUniquePageview = false; // referred by same-url, so not a unique pageview
 			}
 		} else {
-			d.r = document.referrer; // referred by external site, so send referrer URL to be stored
+			referrer = document.referrer; // referred by external site, so send referrer URL to be stored
 		}
 	}
 
@@ -108,12 +105,11 @@ function trackPageview() {
 
 		// update tracking cookie
 		if (useCookie) {
-			if (isUniquePageview) {
-				pagesViewed.push(postId)
+			if (pagesViewed.indexOf(postId) === -1) {
+				pagesViewed.push(postId);
 			}
 			let expires = new Date();
 			expires.setHours(expires.getHours() + 6);
-
 			setCookie('_koko_analytics_pages_viewed', pagesViewed.join(','), {expires, path: '/'})
 		}
 	}
@@ -123,6 +119,12 @@ function trackPageview() {
 	window.setTimeout(finalize, 5000);
 
 	// add to DOM to fire request
+	const d = {
+		p:  postId,
+		nv: isNewVisitor ? 1 : 0,
+		up: isUniquePageview ? 1 : 0,
+		r: referrer,
+	};
 	img.src = trackerUrl + (trackerUrl.indexOf('?') > -1 ? '&' : '?') + stringifyObject(d);
 	document.body.appendChild(img);
 }
