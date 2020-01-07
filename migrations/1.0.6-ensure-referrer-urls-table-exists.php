e98619ee787513fd2cb6fe064eddb4d2308f964b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-01-07 16:26:53 +0100

    ensure referrer table exists, some users reported not having it

---
+++ migrations/1.0.6-ensure-referrer-urls-table-exists.php
@@ -0,0 +1,12 @@
+<?php
+
+defined( 'ABSPATH' ) or exit;
+
+global $wpdb;
+$wpdb->query(
+	"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}koko_analytics_referrer_urls (
+	   id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	   url VARCHAR(255) NOT NULL,
+	   UNIQUE INDEX (url)
+	) ENGINE=INNODB CHARACTER SET=ascii"
+);
