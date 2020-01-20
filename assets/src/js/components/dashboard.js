3d1f43790c14393041258b7304bf737af92b87bc
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-01-20 10:47:27 +0100

    remember view when navigating away from the page, so it re-opens when you revisit. #35

--- assets/src/js/components/dashboard.js
+++ assets/src/js/components/dashboard.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import Chart from './chart.js'
 import Datepicker from './datepicker.js'
 import Totals from './totals.js'
@@ -29,7 +28,12 @@ function parseUrlParams (str) {
 export default class Dashboard extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { ...this.parseStateFromLocation(window.location.hash) }
+    this.state = {
+      startDate: new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0),
+      endDate: new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59),
+      ...this.parseStateFromLocalStorage(),
+      ...this.parseStateFromLocation(window.location.hash)
+    }
     this.setDates = this.setDates.bind(this)
   }
 
@@ -45,24 +49,54 @@ export default class Dashboard extends React.Component {
     this.unlisten()
   }
 
+  parseStateFromLocalStorage () {
+    try {
+      const startDate = localStorage.getItem('start_date')
+      const endDate = localStorage.getItem('end_date')
+
+      if (startDate && endDate) {
+        return {
+          startDate: new Date(startDate),
+          endDate: new Date(endDate + ' 23:59:59')
+        }
+      }
+    } catch (e) {}
+
+    return {}
+  }
+
   parseStateFromLocation (str) {
     const searchPos = str.indexOf('?')
     const queryStr = str.substring(searchPos + 1)
     const params = parseUrlParams(queryStr)
 
-    return {
-      startDate: params.start_date ? new Date(params.start_date) : new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0),
-      endDate: params.end_date ? new Date(params.end_date + ' 23:59:59') : new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59)
+    if (params.start_date && params.end_date) {
+      return {
+        startDate: new Date(params.start_date),
+        endDate: new Date(params.end_date + ' 23:59:59')
+      }
     }
+
+    return {}
   }
 
   setDates (startDate, endDate) {
     if (startDate.getTime() === this.state.startDate.getTime() && endDate.getTime() === this.state.endDate.getTime()) {
       return
     }
-
+    
+    // update state
     this.setState({ startDate, endDate })
-    this.props.history.push(`/?start_date=${formatDate(startDate)}&end_date=${formatDate(endDate)}`)
+
+    // update local storage & URL
+    startDate = formatDate(startDate)
+    endDate = formatDate(endDate)
+    try {
+      localStorage.setItem('start_date', startDate)
+      localStorage.setItem('end_date', endDate)
+    } catch (e) {}
+
+    this.props.history.push(`/?start_date=${startDate}&end_date=${endDate}`)
   }
 
   render () {
