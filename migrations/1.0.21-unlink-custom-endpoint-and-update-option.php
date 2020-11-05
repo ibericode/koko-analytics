d08ba5736b3abc88b5e7a9a5421da1213e1e1c19
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-11-05 16:25:57 +0100

    instruct koko not to use custom endpoint because we've unlinked it in the migration

---
+++ migrations/1.0.21-unlink-custom-endpoint-and-update-option.php
@@ -0,0 +1,9 @@
+<?php
+
+// What we should have done in version 1.0.20
+
+// Unlink the custom endpoint file to ensure we get the latest logic for determining whether to use this
+unlink( ABSPATH . '/koko-analytics-collect.php' );
+
+// Update option that says to use custom endpoint, this will be recalculated the next time logic for custom endpoint runs
+update_option( 'koko_analytics_use_custom_endpoint', false );
