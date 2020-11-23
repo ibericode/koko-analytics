443ec2affb0658d2f245243a46af44cec370686a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-11-23 09:56:01 +0100

    fix php cs

--- migrations/1.0.2-add-cap-to-administrator-role.php
+++ migrations/1.0.2-add-cap-to-administrator-role.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) or exit;
 
 $role = get_role( 'administrator' );
-if ($role) {
+if ( $role ) {
 	$role->add_cap( 'view_koko_analytics' );
 	$role->add_cap( 'manage_koko_analytics' );
 }
