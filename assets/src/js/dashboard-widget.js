4bd5c0a1f22a91adb98f5841d137191017a331c4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-15 03:41:52 +0800

    Delete assets/src directory

--- assets/src/js/dashboard-widget.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import Chart from './components/chart'
-import { render, h } from 'preact'
-const el = document.getElementById('koko-analytics-dashboard-widget-mount')
-const now = new Date()
-const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14, 0, 0, 0)
-const endDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59)
-render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />, el)
