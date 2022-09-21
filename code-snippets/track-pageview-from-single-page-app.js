72775597f030cf0d58dd8a34fad030f72fc81b0a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-09-21 18:54:45 +0200

    add sample code snippet for calling trackPageview programmatically. #166 #103

---
+++ code-snippets/track-pageview-from-single-page-app.js
@@ -0,0 +1,10 @@
+let postId = -1 // general site visit
+
+// if this is a visit to a specific page or post, we need to supply the post ID to trackPageview
+// so we extract if from the <body> element
+const matches = document.body.className.match(/(postid-|page-id-)(\d+)/)
+if (matches.length === 3) {
+  postId = matches.pop()
+}
+
+window.koko_analytics.trackPageview(postId)
