37249631e8f55111ea5e330d6feee24e0b6a5064
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-04-25 10:37:29 +0200

    only render dashboard widget chart if it is not hidden

--- assets/src/js/dashboard-widget.js
+++ assets/src/js/dashboard-widget.js
@@ -4,4 +4,12 @@ const el = document.getElementById('koko-analytics-dashboard-widget-mount')
 const now = new Date()
 const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14, 0, 0, 0)
 const endDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59)
-render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />, el)
+
+function maybeRender() {
+  if (el.clientWidth) {
+    render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />, el)
+  }
+}
+
+window.jQuery(document).on('postbox-toggled', maybeRender)
+maybeRender();
