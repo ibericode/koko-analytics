3fbfb13376070b47e9002fc206af4eedee7c9276
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-08-30 21:05:11 +0200

    allow specifying multiple post types in get_most_viewed_posts() / koko_analytics_most_viewed_posts. closes #188

--- src/functions.php
+++ src/functions.php
@@ -120,10 +120,15 @@ function get_most_viewed_posts( array $args ) {
 		'show_date' => false,
 		'days'    => 30,
 	);
+
 	$args = array_merge( $default_args, $args );
+	$args['post_type'] = is_array( $args['post_type'] ) ? $args['post_type'] : explode( ',', $args['post_type'] );
 	$start_date = gmdate( 'Y-m-d', strtotime( "-{$args['days']} days" ) );
 	$end_date   = gmdate( 'Y-m-d', strtotime( 'tomorrow midnight' ) );
-	$sql        = $wpdb->prepare( "SELECT p.id, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_post_stats s JOIN {$wpdb->posts} p ON s.id = p.id WHERE p.id > 0 AND s.date >= %s AND s.date <= %s AND p.post_type = %s AND p.post_status = 'publish' GROUP BY s.id ORDER BY pageviews DESC LIMIT 0, %d", array( $start_date, $end_date, $args['post_type'], $args['number'] ) );
+	$post_types = join(',', array_map( function( $v ) {
+		return "'" . esc_sql( $v ) . "'";
+	}, $args['post_type'] ) );
+	$sql        = $wpdb->prepare( "SELECT p.id, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_post_stats s JOIN {$wpdb->posts} p ON s.id = p.id WHERE p.id > 0 AND s.date >= %s AND s.date <= %s AND p.post_type IN ($post_types) AND p.post_status = 'publish' GROUP BY s.id ORDER BY pageviews DESC LIMIT 0, %d", array( $start_date, $end_date, $args['number'] ) );
 	$results    = $wpdb->get_results( $sql );
 	if ( empty( $results ) ) {
 		return array();
