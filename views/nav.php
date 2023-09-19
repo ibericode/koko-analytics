499430eb0b5fd230928163ae6220824b1f8c124e
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-09-19 15:07:01 +0200

    hide entire admin nav menu if there's no options

--- views/nav.php
+++ views/nav.php
@@ -3,17 +3,18 @@
 * @var string $tab
 */
 ?>
+<?php if (current_user_can('manage_koko_analytics')) { ?>
 <ul class="ka-admin-nav subsubsub">
-	<li><a href="<?php echo admin_url('index.php?page=koko-analytics'); ?>"	
+	<li><a href="<?php echo admin_url('index.php?page=koko-analytics'); ?>"
 							<?php
 							if ($tab === 'dashboard') {
 								?>
 		 class="current"<?php } ?>><?php echo __('Stats', 'koko-analytics'); ?></a></li>
-	<?php if (current_user_can('manage_koko_analytics')) { ?>
-		<li><a href="<?php echo admin_url('index.php?page=koko-analytics&tab=settings'); ?>" 
+
+		<li><a href="<?php echo admin_url('index.php?page=koko-analytics&tab=settings'); ?>"
 								<?php
 								if ($tab === 'settings') {
 									?>
 			class="current"<?php } ?>><?php echo __('Settings', 'koko-analytics'); ?></a></li>
-	<?php } ?>
 </ul>
+<?php } ?>
