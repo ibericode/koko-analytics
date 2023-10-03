1b25c0c816cf472adc06127b1a58cb059560724b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-03 12:36:53 +0200

    maybe don't hardcode table prefixes, okay Danny?

--- src/class-rest.php
+++ src/class-rest.php
@@ -142,7 +142,7 @@ class Rest {
 		$start_date         = $params['start_date'] ?? gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		$end_date           = $params['end_date'] ?? gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		$date_format        = $params['monthly'] ? '%Y-%m' : '%Y-%m-%d';
-		$sql                = $wpdb->prepare( 'SELECT DATE_FORMAT(d.date, %s) AS date, COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM wp_koko_analytics_dates d LEFT JOIN wp_koko_analytics_site_stats s ON s.date = d.date WHERE d.date >= %s AND d.date <= %s GROUP BY date', array( $date_format, $start_date, $end_date ) );
+		$sql                = $wpdb->prepare( "SELECT DATE_FORMAT(d.date, %s) AS date, COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM {$wpdb->prefix}koko_analytics_dates d LEFT JOIN {$wpdb->prefix}koko_analytics_site_stats s ON s.date = d.date WHERE d.date >= %s AND d.date <= %s GROUP BY date", array( $date_format, $start_date, $end_date ) );
 		$result             = $wpdb->get_results( $sql );
 		$result             = is_array( $result ) ? array_map(function ( $row ) {
 			$row->pageviews = (int) $row->pageviews;
@@ -173,16 +173,16 @@ class Rest {
 		}
 		$previous_start_date = gmdate('Y-m-d', strtotime($start_date) - ( strtotime($end_date . ' 23:59:59') - strtotime($start_date) ));
 
-		$sql                = $wpdb->prepare( 'SELECT
+		$sql                = $wpdb->prepare( "SELECT
 			        cur.*,
 			        cur.visitors - prev.visitors AS visitors_change,
 			        cur.pageviews - prev.pageviews AS pageviews_change,
 			        cur.visitors / prev.visitors - 1 AS visitors_change_rel,
 			        cur.pageviews / prev.pageviews - 1 AS pageviews_change_rel
 			    FROM
-			        (SELECT COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM wp_koko_analytics_site_stats s WHERE s.date >= %s AND s.date <= %s) AS cur,
-			        (SELECT COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM wp_koko_analytics_site_stats s WHERE s.date >= %s AND s.date < %s) AS prev;
-			', array( $start_date, $end_date, $previous_start_date, $start_date ) );
+			        (SELECT COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM {$wpdb->prefix}koko_analytics_site_stats s WHERE s.date >= %s AND s.date <= %s) AS cur,
+			        (SELECT COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM {$wpdb->prefix}koko_analytics_site_stats s WHERE s.date >= %s AND s.date < %s) AS prev;
+			", array( $start_date, $end_date, $previous_start_date, $start_date ) );
 		$result             = $wpdb->get_row( $sql );
 		$send_cache_headers = $end_date < gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		return $this->respond( $result, $send_cache_headers );
