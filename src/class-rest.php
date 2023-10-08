3a2899bd36cf1fab2279ab3b4e11365ae9627cd5
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-08 14:36:32 +0200

    unify logic for determining whether a date period is in the past

--- src/class-rest.php
+++ src/class-rest.php
@@ -114,10 +114,19 @@ class Rest {
 		);
 	}
 
-	private function respond( $data, bool $send_cache_headers = false ) {
+	private function is_request_for_completed_date_range(\WP_REST_Request $request) : bool {
+		$end_date = $request->get_param('end_date');
+		if ($end_date === null) {
+			return false;
+		}
+
+		$today = gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		return $end_date < $today;
+	}
+
+	private function respond( $data, bool $send_cache_headers = false ) : \WP_REST_Response {
 		$result = new \WP_REST_Response( $data, 200 );
 
-		// if this request was for stats for a closed (past) period
 		// instruct browsers to cache the response for 7 days
 		if ( $send_cache_headers ) {
 			$result->set_headers( array( 'Cache-Control' => 'max-age=604800' ) );
@@ -149,7 +158,7 @@ class Rest {
 			$row->visitors  = (int) $row->visitors;
 			return $row;
 		}, $result) : $result;
-		$send_cache_headers = WP_DEBUG === false && $end_date < gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$send_cache_headers = WP_DEBUG === false && $this->is_request_for_completed_date_range($request);
 		return $this->respond( $result, $send_cache_headers );
 	}
 
@@ -163,8 +172,8 @@ class Rest {
 	public function get_totals( \WP_REST_Request $request ) {
 		global $wpdb;
 		$params     = $request->get_query_params();
-		$start_date = isset( $params['start_date'] ) ? $params['start_date'] : gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
-		$end_date   = isset( $params['end_date'] ) ? $params['end_date'] : gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$start_date = $params['start_date'] ?? gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$end_date   = $params['end_date'] ?? gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 
 		// if end date is a future date, cap it at today so that relative differences to previous period are fair
 		$today = gmdate('Y-m-d' );
@@ -184,7 +193,7 @@ class Rest {
 			        (SELECT COALESCE(SUM(visitors), 0) AS visitors, COALESCE(SUM(pageviews), 0) AS pageviews FROM {$wpdb->prefix}koko_analytics_site_stats s WHERE s.date >= %s AND s.date < %s) AS prev;
 			", array( $start_date, $end_date, $previous_start_date, $start_date ) );
 		$result             = $wpdb->get_row( $sql );
-		$send_cache_headers = $end_date < gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$send_cache_headers = WP_DEBUG === false && $this->is_request_for_completed_date_range($request);
 		return $this->respond( $result, $send_cache_headers );
 	}
 
@@ -197,16 +206,15 @@ class Rest {
 	 */
 	public function get_posts( \WP_REST_Request $request ) {
 		global $wpdb;
+		$send_cache_headers = WP_DEBUG === false && $this->is_request_for_completed_date_range($request);
+
 		$params     = $request->get_query_params();
-		$start_date = isset( $params['start_date'] ) ? $params['start_date'] : gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
-		$end_date   = isset( $params['end_date'] ) ? $params['end_date'] : gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$start_date = $params['start_date'] ?? gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$end_date   = $params['end_date'] ?? gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		$offset     = isset( $params['offset'] ) ? absint( $params['offset'] ) : 0;
 		$limit      = isset( $params['limit'] ) ? absint( $params['limit'] ) : 10;
 		$sql        = $wpdb->prepare( "SELECT s.id, SUM(visitors) AS visitors, SUM(pageviews) AS pageviews, COALESCE(NULLIF(p.post_title, ''), p.post_name) AS post_title FROM {$wpdb->prefix}koko_analytics_post_stats s LEFT JOIN {$wpdb->posts} p ON p.ID = s.id WHERE s.date >= %s AND s.date <= %s GROUP BY s.id ORDER BY pageviews DESC, s.id ASC LIMIT %d, %d", array( $start_date, $end_date, $offset, $limit ) );
 		$results    = $wpdb->get_results( $sql );
-		if ( empty( $results ) ) {
-			return $this->respond( array() );
-		}
 
 		// add permalink to each result
 		$results = array_map( function( $row ) {
@@ -224,7 +232,6 @@ class Rest {
 			return $row;
 		}, $results);
 
-		$send_cache_headers = $end_date < gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		return $this->respond( $results, $send_cache_headers );
 	}
 
@@ -238,14 +245,13 @@ class Rest {
 	public function get_referrers( \WP_REST_Request $request ) {
 		global $wpdb;
 		$params     = $request->get_query_params();
-		$start_date = isset( $params['start_date'] ) ? $params['start_date'] : gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
-		$end_date   = isset( $params['end_date'] ) ? $params['end_date'] : gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$start_date = $params['start_date'] ?? gmdate( 'Y-m-d', strtotime( '1st of this month' ) + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$end_date   = $params['end_date'] ?? gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
 		$offset     = isset( $params['offset'] ) ? absint( $params['offset'] ) : 0;
 		$limit      = isset( $params['limit'] ) ? absint( $params['limit'] ) : 10;
 		$sql        = $wpdb->prepare( "SELECT s.id, url, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_referrer_stats s JOIN {$wpdb->prefix}koko_analytics_referrer_urls r ON r.id = s.id WHERE s.date >= %s AND s.date <= %s GROUP BY s.id ORDER BY pageviews DESC, r.id ASC LIMIT %d, %d", array( $start_date, $end_date, $offset, $limit ) );
 		$results    = $wpdb->get_results( $sql );
-
-		$send_cache_headers = $end_date < gmdate( 'Y-m-d', time() + get_option( 'gmt_offset', 0 ) * HOUR_IN_SECONDS );
+		$send_cache_headers = WP_DEBUG === false && $this->is_request_for_completed_date_range($request);
 		return $this->respond( $results, $send_cache_headers );
 	}
 
