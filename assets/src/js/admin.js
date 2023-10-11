d71137cd1388b6cbedfef91847f8ca2a6f8e6b73
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-11 13:45:43 +0200

    fix compatibility with WordPress < 6.2 (b/c it ships React v17)

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -1,9 +1,14 @@
 import Dashboard from './components/dashboard'
 import './globals.js'
 import React from 'react'
-import {createRoot} from 'react-dom'
+// eslint-disable-next-line react/no-deprecated
+import {createRoot, render} from 'react-dom'
 import "../css/dashboard.css"
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('koko-analytics-mount')).render(<Dashboard />)
+  if (typeof createRoot === 'function') {
+    createRoot(document.getElementById('koko-analytics-mount')).render(<Dashboard />)
+  } else {
+    render(<Dashboard />, document.getElementById('koko-analytics-mount'))
+  }
 })
--- assets/src/js/dashboard-widget.js
+++ assets/src/js/dashboard-widget.js
@@ -4,14 +4,19 @@ const el = document.getElementById('koko-analytics-dashboard-widget-mount')
 const now = new Date()
 const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14, 0, 0, 0)
 const endDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59)
-import {createRoot} from 'react-dom'
+// eslint-disable-next-line react/no-deprecated
+import {createRoot, render} from 'react-dom'
 
 function maybeRender() {
   if (!el.clientWidth) {
     return;
   }
 
-  createRoot(el).render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />)
+  if (typeof createRoot === 'function') {
+    createRoot(el).render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />)
+  } else {
+    render(<Chart startDate={startDate} endDate={endDate} height={200} width={el.clientWidth} />, el)
+  }
 }
 
 window.jQuery(document).on('postbox-toggled', maybeRender)
