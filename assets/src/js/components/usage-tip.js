c07f4e79308b7044e612b0abf9501eb41339fd27
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-16 11:45:04 +0200

    set-up -> set up

--- assets/src/js/components/usage-tip.js
+++ assets/src/js/components/usage-tip.js
@@ -5,7 +5,7 @@ let tips = [
   __('Tip: use the arrow keys on your keyboard to cycle through date ranges.', 'koko-analytics'),
   __('Tip: you can set a default date range in the plugin settings.', 'koko-analytics'),
   __('Tip: did you know there is a widget, shortcode and template function to <a href="%1s">show a list of the most viewed posts</a> on your site?', 'koko-analytics').replace('%1s', 'https://www.kokoanalytics.com/kb/showing-most-viewed-posts-on-your-wordpress-site/'),
-  __('Tip: Use <a href="%1s">Koko Analytics Pro</a> to set-up custom event tracking.', 'koko-analytics').replace('%1s', 'https://www.kokoanalytics.com/pricing/'),
+  __('Tip: Use <a href="%1s">Koko Analytics Pro</a> to set up custom event tracking.', 'koko-analytics').replace('%1s', 'https://www.kokoanalytics.com/pricing/'),
 ]
 
 export function UsageTip() {
