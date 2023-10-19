835aacf6ef9c23bd698d511f7f00780ab9ee933a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-19 21:26:47 +0200

    2nd argument to setTimeout takes ms, not s...

--- assets/src/js/util/api.js
+++ assets/src/js/util/api.js
@@ -49,4 +49,6 @@ export function request (path, opts = {}) {
 
 // Nonces are valid for 24 hours, whereas WP Admin sessions can live for much longer.
 // So here we reload the page every 12 hours so that we get a new X-WP-Nonce that is guaranteed to work.
-window.setTimeout(window.location.reload, 12 * 3600);
+window.setTimeout(() => {
+  window.location.reload()
+}, 12 * 60 * 60 * 1000);
