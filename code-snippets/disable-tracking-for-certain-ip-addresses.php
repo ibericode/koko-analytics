49abc7b4ecb4da89e12cc2b64790862155903fdc
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-25 20:27:19 +0200

    add sample snippet to disable tracking for certain ip addresses. #6

---
+++ code-snippets/disable-tracking-for-certain-ip-addresses.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Koko Analytics: Disable tracking for certain IP addresses
+ */
+
+add_filter('koko_analytics_load_tracking_script', function () {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $excluded_ip_addresses = array(
+        '127.0.0.1',
+        '254.123.182.120',
+    );
+    if (in_array($ip_address, $excluded_ip_addresses, true)) {
+        return false;
+    }
+
+    return true;
+});
