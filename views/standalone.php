8fd2e5991b00d5d6717127a6d08cc4365bb32cf7
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-29 16:36:27 +0100

    add action hook on standalone dashboard for pro to hook into

--- views/standalone.php
+++ views/standalone.php
@@ -8,6 +8,7 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
     <link rel="stylesheet" href="<?php echo esc_attr(plugins_url('assets/dist/css/standalone.css', KOKO_ANALYTICS_PLUGIN_FILE)); ?>">
     <link rel="stylesheet" href="<?php echo esc_attr(plugins_url('assets/dist/css/admin.css', KOKO_ANALYTICS_PLUGIN_FILE)); ?>">
     <?php wp_print_scripts('koko-analytics-admin'); ?>
+    <?php do_action('koko_analytics_standalone_head'); ?>
     <meta name="charset" content="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="referrer" content="no-referrer-when-downgrade">
