e83f4ad454c1072ed61cc0085fa9952198b27400
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-29 16:29:07 +0100

    print in header for standalone too

--- views/standalone.php
+++ views/standalone.php
@@ -7,6 +7,7 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
 <head>
     <link rel="stylesheet" href="<?php echo esc_attr(plugins_url('assets/dist/css/standalone.css', KOKO_ANALYTICS_PLUGIN_FILE)); ?>">
     <link rel="stylesheet" href="<?php echo esc_attr(plugins_url('assets/dist/css/admin.css', KOKO_ANALYTICS_PLUGIN_FILE)); ?>">
+    <?php wp_print_scripts('koko-analytics-admin'); ?>
     <meta name="charset" content="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="referrer" content="no-referrer-when-downgrade">
@@ -15,7 +16,6 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
 </head>
 <body>
     <?php $this->show_dashboard_page(); ?>
-    <?php wp_print_scripts('koko-analytics-admin'); ?>
     <?php do_action('koko_analytics_standalone_footer'); ?>
 </body>
 </html>
