6fd7840ae1f613c31b17164e124fb730725f4871
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-31 08:03:46 +0100

    get rid of standalone hooks now we've consolidated everything into dashboard

--- src/views/standalone.php
+++ src/views/standalone.php
@@ -5,9 +5,6 @@
 defined('ABSPATH') or exit; ?><!DOCTYPE html>
 <html lang="<?php bloginfo('language'); ?>">
 <head>
-    <?php wp_print_scripts('koko-analytics-dashboard'); ?>
-    <?php do_action('koko_analytics_dashboard_head'); ?>
-    <link rel="stylesheet" href="<?php echo esc_attr(plugins_url('assets/dist/css/dashboard.css', KOKO_ANALYTICS_PLUGIN_FILE)); ?>">
     <meta name="charset" content="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="referrer" content="no-referrer-when-downgrade">
@@ -16,6 +13,5 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
 </head>
 <body class="ka-dashboard">
     <?php $this->show(); ?>
-    <?php do_action('koko_analytics_dashboard_footer'); ?>
 </body>
 </html>
