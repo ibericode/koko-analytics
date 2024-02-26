7d07e361add7fc5319e285ac3cf6befe58180877
Danny van Kooten <hi@dvk.co>
2024-02-26 11:41:50 +0100

    add transient caching & use Stats class for fetching count

--- src/class-shortcode-site-counter.php
+++ src/class-shortcode-site-counter.php
@@ -1,81 +1,51 @@
 <?php
+
 /**
  * @package koko-analytics
  * @license GPL-3.0+
- * @author Anil Kulkarni
- * Adds support for a shortcode to display the number of times a page or a site has been viewed
- * For example, to show the last 30 day stats in the footer
- * <?php
- *   echo do_shortcode('[koko_analytics_site_counter days=30]');
- * ?>
+ * @author Anil Kulkarni, Danny van Kooten
  *
+ * Adds support for a shortcode to display the number of times a page or a site has been viewed
+
  * Options:
- * days: How many previous days to count. Defaults to -1 which means show views for all time
- * use_pageviews: If you want to counte pageviews instead of visitors, set this to True
- * always_show_global_stats: By default, the value of the counter will depend on the page.
- *     If you're viewing a single page, it shows the stats only for that page, else the whole site.
- *     Setting always_show_global_stats to True means the stats will always be for the whole site
+ *  days: How many previous days to count. Defaults to -1 which means show views for all time
+ *  metric: Either "pageviews" or "visitors"
+ *  global: Set to true to show count for entire site instead of the current page.
  */
 
 namespace KokoAnalytics;
 
-class ShortCode_Site_Counter {
-	const SHORTCODE = 'koko_analytics_site_counter';
+class ShortCode_Site_Counter
+{
+    const SHORTCODE = 'koko_analytics_counter';
 
-	public function init() {
-		add_shortcode( self::SHORTCODE, array( $this, 'content' ) );
-	}
+    public function init()
+    {
+        add_shortcode(self::SHORTCODE, array($this, 'content'));
+    }
 
-	public function content( $args ) {
-		$default_args = array(
-		'days' => -1,
-		'use_pageviews' => false,
-		'always_show_global_stats' => false,
-		);
-		$args = shortcode_atts( $default_args, $args, self::SHORTCODE );
-		$days = $args['days'];
-		# Since the column value is directly in the sql query, hard code it
-		# to prevent SQL injection attacks.
-		$column = $args['use_pageviews'] === true ? 'pageviews' : 'visitors';
+    public function content($args)
+    {
+        $default_args = array(
+            'days' => 365 * 10,
+            'metric' => 'visitors',
+            'global' => false,
+        );
+        $args = shortcode_atts($default_args, $args, self::SHORTCODE);
 
-		if ( ! is_single() || $args['always_show_global_stats'] === true ) {
-			$count = $this->get_site_views( $days, $column );
-		} else {
-			$count = $this->get_single_page_views( $days, $column );
-		}
+        $id = $args['global'] ? 0 : (int) get_the_ID();
+        $start_date = create_local_datetime("-{$args['days']} days")->format('Y-m-d');
+        $end_date = create_local_datetime('tomorrow midnight')->format('Y-m-d');
 
-		$html = sprintf( PHP_EOL . ' <span class="koko-analytics-post-count">%s</span>', $count );
-		return $html;
-	}
+        $cache_key = 'ka_counter_' . $id . $args['metric'][0] . $args['days'];
+        $count = get_transient($cache_key);
+        if (!$count) {
+            $stats = new Stats();
+            $totals = $stats->get_totals($start_date, $end_date, $id);
+            $count = $args['metric'] == 'pageviews' ? $totals->pageviews : $totals->visitors;
+            set_transient($cache_key, $count, 60);
+        }
 
-	private function get_start_date( $days ) {
-		$timezone = get_option( 'timezone_string', 'UTC' );
-			$datetime = new \DateTime( 'now', new \DateTimeZone( $timezone ) );
-			$datetime->modify( sprintf( '-%d days', $days ) );
-			$start_date = $datetime->format( 'Y-m-d' );
-			return $start_date;
-	}
-
-	private function get_site_views( $days, $column ) {
-		global $wpdb;
-		if ( $days == -1 ) {
-				$sql = "SELECT SUM({$column}) FROM {$wpdb->prefix}koko_analytics_site_stats";
-		} else {
-				$start_date = $this->get_start_date( $days );
-				$sql = $wpdb->prepare( "SELECT SUM({$column}) FROM {$wpdb->prefix}koko_analytics_site_stats s WHERE s.date >= %s", array( $start_date ) );
-		}
-		return (int) ( $wpdb->get_var( $sql ) ?? 0 );
-	}
-
-	private function get_single_page_views( $days, $column ) {
-		global $wpdb;
-		if ( $days == -1 ) {
-				$sql = $wpdb->prepare( "SELECT SUM({$column}) FROM {$wpdb->prefix}koko_analytics_post_stats s WHERE s.id = %d", array( get_the_ID() ) );
-		} else {
-				$start_date = $this->get_start_date( $days );
-				$sql = $wpdb->prepare( "SELECT SUM({$column}) FROM {$wpdb->prefix}koko_analytics_post_stats s WHERE s.id = %d AND s.date >= %s", array( get_the_ID(), $start_date ) );
-		}
-		return (int) ( $wpdb->get_var( $sql ) ?? 0 );
-
-	}
+        return '<span class="koko-analytics-counter">' . $count . '</span>';
+    }
 }
