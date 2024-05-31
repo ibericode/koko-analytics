20cf6dc4044398d0d94daf81b6194b364737def3
Danny van Kooten <hi@dvk.co>
2024-05-31 20:08:53 +0200

    remove dependency on jest

--- assets/src/js/util/numbers.test.js
+++ assets/src/js/util/numbers.test.js
@@ -1,53 +1,67 @@
 import {magnitude, formatLargeNumber} from './numbers.js';
 
+function assert_equals(left, right) {
+  if (left !== right) {
+    throw Error(`Failed asserting that ${left} equals ${right}`);
+  }
+}
 
-// eslint-disable-file no-undef
-test('formatLargeNumber works', () => {
-  expect(formatLargeNumber(1000)).toBe('1000')
-  expect(formatLargeNumber(10000)).toBe('10K')
-  expect(formatLargeNumber(100000)).toBe('100K')
-  expect(formatLargeNumber(1000000)).toBe('1M')
-  expect(formatLargeNumber(10000000)).toBe('10M')
-  expect(formatLargeNumber(170000)).toBe('170K')
-  expect(formatLargeNumber(17000)).toBe('17K')
-  expect(formatLargeNumber(1700)).toBe('1700')
-  expect(formatLargeNumber(170)).toBe('170')
-  expect(formatLargeNumber(175500)).toBe('176K')
-  expect(formatLargeNumber(17550)).toBe('17.6K')
-  expect(formatLargeNumber(1755)).toBe('1755')
-  expect(formatLargeNumber(175)).toBe('175')
-  expect(formatLargeNumber(9999)).toBe('9999')
-  expect(formatLargeNumber(99999)).toBe('100K')
-  expect(formatLargeNumber(999999)).toBe('1000K')
-  expect(formatLargeNumber(9999990)).toBe('10M')
-})
+[
+  [0, '0'],
+  [1, '1'],
+  [1000, '1000'],
+  [10000, '10K'],
+  [100000, '100K'],
+  [1000000, '1M'],
+  [10000000, '10M'],
+  [170000, '170K'],
+  [17000, '17K'],
+  [1700, '1700'],
+  [170, '170'],
+  [17, '17'],
+  [175500, '176K'],
+  [17550, '17.6K'],
+  [1755, '1755'],
+  [175, '175'],
+  [9999, '9999'],
+  [99999, '100K'],
+  [999999, '1000K'],
+  [9999990, '10M'],
+].forEach((args) => {
 
-test('magnitude works properly', () => {
-  [
-    [1, 10],
-    [2, 10],
-    [3, 10],
-    [4, 10],
-    [6, 10],
-    [7, 10],
-    [8, 10],
-    [9, 10],
-    [10, 10],
-    [11, 20],
-    [21, 30],
-    [24, 30],
-    [99, 100],
-    [100, 100],
-    [101, 200],
-    [1001, 2000],
-    [1200, 2000],
-    [20000, 20000],
-    [20001, 30000],
-    [100000, 100000],
-    [101000, 110000],
-    [151000, 160000],
-  ].forEach((args) => {
-    const [n, expected] = args
-    expect(magnitude(n)).toBe(expected)
-  })
+  const left = formatLargeNumber(args[0]);
+  const right = args[1];
+  console.log((left == right ? "OK" : "FAIL") + "    formatLargeNumber("+args[0]+")");
+  assert_equals(left, right);
+});
+
+
+[
+  [1, 10],
+  [2, 10],
+  [3, 10],
+  [4, 10],
+  [6, 10],
+  [7, 10],
+  [8, 10],
+  [9, 10],
+  [10, 10],
+  [11, 20],
+  [21, 30],
+  [24, 30],
+  [99, 100],
+  [100, 100],
+  [101, 200],
+  [1001, 2000],
+  [1200, 2000],
+  [20000, 20000],
+  [20001, 30000],
+  [100000, 100000],
+  [101000, 110000],
+  [151000, 160000],
+].forEach((args) => {
+  const left = magnitude(args[0]);
+  const right = args[1];
+  console.log((left == right ? "OK" : "FAIL") + "    magnitude("+args[0]+")");
+  assert_equals(left, right);
 })
