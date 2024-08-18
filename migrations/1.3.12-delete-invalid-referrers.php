0ee08e2b99f560f547405c3fa87b0bcca635c50f
Danny van Kooten <hi@dvk.co>
2024-08-18 09:11:09 +0200

    delete invalid referrer data on upgrade

---
+++ migrations/1.3.12-delete-invalid-referrers.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('ABSPATH') or exit;
+
+global $wpdb;
+
+$site_url = get_site_url();
+
+$wpdb->query($wpdb->prepare("DELETE s, u FROM {$wpdb->prefix}koko_analytics_referrer_stats s LEFT JOIN {$wpdb->prefix}koko_analytics_referrer_urls u ON s.id = u.id WHERE u.url LIKE %s;", [$site_url . '%']));
