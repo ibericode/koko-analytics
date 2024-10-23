debc963330b9d8afe58c2b4b43baee1e497f81bd
Danny van Kooten <hi@dvk.co>
2024-10-23 15:44:32 +0200

    get rid of active_plugins option on plugin activation, need to do it more often anyway

--- src/class-plugin.php
+++ src/class-plugin.php
@@ -22,37 +22,13 @@ class Plugin
     {
         $this->aggregator = $aggregator;
 
-        register_activation_hook(KOKO_ANALYTICS_PLUGIN_FILE, array($this, 'on_activation'));
-        add_filter('pre_update_option_active_plugins', array($this, 'filter_active_plugins'), 10, 1);
-        add_action('init', array($this, 'maybe_run_db_migrations'), 10, 0);
-        add_action('init', array($this, 'maybe_run_actions'), 10, 0);
-    }
-
-    /**
-     * This method moves Koko Analytics to the front of the list of currently active plugins.
-     * This improves performance if not using the optimized endpoint.
-     *
-     * @param array $plugins
-     * @return array
-     */
-    public function filter_active_plugins($plugins)
-    {
-        if (empty($plugins)) {
-            return $plugins;
-        }
-
-        $pattern = '/' . preg_quote(plugin_basename(KOKO_ANALYTICS_PLUGIN_FILE), '/') . '$/';
-        return array_merge(
-            preg_grep($pattern, $plugins),
-            preg_grep($pattern, $plugins, PREG_GREP_INVERT)
-        );
+        register_activation_hook(KOKO_ANALYTICS_PLUGIN_FILE, [$this, 'on_activation']);
+        add_action('init', [$this, 'maybe_run_db_migrations'], 10, 0);
+        add_action('init', [$this, 'maybe_run_actions'], 10, 0);
     }
 
     public function on_activation(): void
     {
-        // make sure koko analytics loads first to prevent unnecessary work on stat collection requests
-        update_option('active_plugins', get_option('active_plugins'));
-
         // add capabilities to administrator role (if it exists)
         $role = get_role('administrator');
         if ($role instanceof \WP_User) {
