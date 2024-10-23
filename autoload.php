58db971c597bcd6cec20967c72fe45d1e3fde922
Danny van Kooten <hi@dvk.co>
2024-10-23 07:54:09 +0200

    minor memory usage improvements by not storing absolute directory for each classmap entry

--- autoload.php
+++ autoload.php
@@ -5,27 +5,27 @@ require __DIR__ . '/src/global-functions.php';
 
 spl_autoload_register(function($class) {
     static $classmap = [
-        'KokoAnalytics\\Admin' => __DIR__ . '/src/class-admin.php',
-        'KokoAnalytics\\Aggregator' => __DIR__ . '/src/class-aggregator.php',
-        'KokoAnalytics\\Command' => __DIR__ . '/src/class-command.php',
-        'KokoAnalytics\\Chart_View' => __DIR__ . '/src/class-chart-view.php',
-        'KokoAnalytics\\Dashboard' => __DIR__ . '/src/class-dashboard.php',
-        'KokoAnalytics\\Dashboard_Widget' => __DIR__ . '/src/class-dashboard-widget.php',
-        'KokoAnalytics\\Dates' => __DIR__ . '/src/class-dates.php',
-        'KokoAnalytics\\Endpoint_Installer' => __DIR__ . '/src/class-endpoint-installer.php',
-        'KokoAnalytics\\Migrations' => __DIR__ . '/src/class-migrations.php',
-        'KokoAnalytics\\Pageview_Aggregator' => __DIR__ . '/src/class-pageview-aggregator.php',
-        'KokoAnalytics\\Plugin' => __DIR__ . '/src/class-plugin.php',
-        'KokoAnalytics\\Pruner' => __DIR__ . '/src/class-pruner.php',
-        'KokoAnalytics\\Rest' => __DIR__ . '/src/class-rest.php',
-        'KokoAnalytics\\Script_Loader' => __DIR__ . '/src/class-script-loader.php',
-        'KokoAnalytics\\ShortCode_Site_Counter' => __DIR__ . '/src/class-shortcode-site-counter.php',
-        'KokoAnalytics\\Shortcode_Most_Viewed_Posts' => __DIR__ . '/src/class-shortcode-most-viewed-posts.php',
-        'KokoAnalytics\\Stats' => __DIR__ . '/src/class-stats.php',
-        'KokoAnalytics\\Widget_Most_Viewed_Posts' => __DIR__ . '/src/class-widget-most-viewed-posts.php',
+        'KokoAnalytics\\Admin' => '/src/class-admin.php',
+        'KokoAnalytics\\Aggregator' => '/src/class-aggregator.php',
+        'KokoAnalytics\\Command' => '/src/class-command.php',
+        'KokoAnalytics\\Chart_View' => '/src/class-chart-view.php',
+        'KokoAnalytics\\Dashboard' => '/src/class-dashboard.php',
+        'KokoAnalytics\\Dashboard_Widget' => '/src/class-dashboard-widget.php',
+        'KokoAnalytics\\Dates' => '/src/class-dates.php',
+        'KokoAnalytics\\Endpoint_Installer' => '/src/class-endpoint-installer.php',
+        'KokoAnalytics\\Migrations' => '/src/class-migrations.php',
+        'KokoAnalytics\\Pageview_Aggregator' => '/src/class-pageview-aggregator.php',
+        'KokoAnalytics\\Plugin' => '/src/class-plugin.php',
+        'KokoAnalytics\\Pruner' => '/src/class-pruner.php',
+        'KokoAnalytics\\Rest' => '/src/class-rest.php',
+        'KokoAnalytics\\Script_Loader' => '/src/class-script-loader.php',
+        'KokoAnalytics\\ShortCode_Site_Counter' => '/src/class-shortcode-site-counter.php',
+        'KokoAnalytics\\Shortcode_Most_Viewed_Posts' => '/src/class-shortcode-most-viewed-posts.php',
+        'KokoAnalytics\\Stats' => '/src/class-stats.php',
+        'KokoAnalytics\\Widget_Most_Viewed_Posts' => '/src/class-widget-most-viewed-posts.php',
     ];
 
     if (isset($classmap[$class])) {
-        require $classmap[$class];
+        require __DIR__ . $classmap[$class];
     }
 });
