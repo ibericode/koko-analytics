9cdfe37b601dd3721b24189ff119de69b466471d
Danny van Kooten <hi@dvk.co>
2024-10-30 09:38:08 +0100

    handle response errors

--- src/views/settings-page.php
+++ src/views/settings-page.php
@@ -212,8 +212,10 @@ $public_dashboard_url = add_query_arg(['koko-analytics-dashboard' => 1], home_ur
                 $response = wp_remote_get('https://www.kokoanalytics.com/wp-json/wp/v2/posts?per_page=5');
                 if ($response && wp_remote_retrieve_response_code($response) == 200) {
                     $body = wp_remote_retrieve_body($response);
-                    $posts = json_decode($body);
+                    $posts = json_decode($body) ?? array();
                 } else {
+                    // store empty array to prevent doing an HTTP request on every page load
+                    // we'll try again in 8 hours
                     $posts = array();
                 }
                 set_transient('koko_analytics_remote_posts', $posts, HOUR_IN_SECONDS * 8);
