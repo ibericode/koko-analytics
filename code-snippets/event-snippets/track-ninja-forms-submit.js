ba782c9c17382cec161f02f069c48c8359d8d209
Danny van Kooten <hi@dvk.co>
2025-01-08 10:04:27 +0100

    add ninja forms snippet

---
+++ code-snippets/event-snippets/track-ninja-forms-submit.js
@@ -0,0 +1,15 @@
+// This code snippet is to track submit events coming from a Ninja Forms form
+// Modified from https://developer.ninjaforms.com/codex/listening-to-submit-response/
+
+var mySubmitController = Marionette.Object.extend( {
+  initialize: function() {
+    this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
+  },
+  actionSubmit: function( response ) {
+      koko_analytics.trackEvent('Form submit', 'Page: ' + location.pathname + '; Form: ' + response.data.settings.title);
+  },
+});
+
+jQuery( document ).ready(function() {
+    new mySubmitController();
+});
