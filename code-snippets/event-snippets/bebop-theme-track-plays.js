332e792c3323cc22512bfc4a624208ebd262eff5
Danny van Kooten <hi@dvk.co>
2025-01-10 08:16:53 +0100

    add some text to event snippet bebop

--- code-snippets/event-snippets/bebop-theme-track-plays.js
+++ code-snippets/event-snippets/bebop-theme-track-plays.js
@@ -1,3 +1,7 @@
+// Event snippet for Koko Analytics Pro to track all clicks on play buttons in the Bebop theme
+// This specific example tracks events with the name "Podcast Play"
+// Theme: https://themeforest.net/item/bepop-nonstop-music-wordpress-theme/24075935
+
 document.addEventListener('click', function(evt) {
   if (evt.target.classList && evt.target.classList.contains("btn-play")) {
     window.koko_analytics.trackEvent("Podcast Play", evt.target.getAttribute("data-play-id"));
