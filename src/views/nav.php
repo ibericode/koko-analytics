d89a59349b22ab56e475be31d4cb5c37e0f5f228
Alex Lion <learnwithalex@gmail.com>
2025-01-16 18:36:48 -0800

    Improve I18N Issues based on 1.5.5

--- src/views/nav.php
+++ src/views/nav.php
@@ -9,21 +9,21 @@
     <?php if ($tab === 'dashboard') { ?>
         <?php if (isset($_GET['koko-analytics-dashboard'])) { ?>
             <li><a href="<?php echo admin_url('index.php?page=koko-analytics'); ?>">
-                <?php echo esc_html__('Switch to admin view', 'koko-analytics'); ?>
+                <?php esc_html_e('Switch to admin view', 'koko-analytics'); ?>
             </a></li>
         <?php } else { ?>
             <li><a href="<?php echo esc_attr(add_query_arg(['koko-analytics-dashboard' => 1], home_url())); ?>"">
-                <?php echo esc_html__('Switch to standalone view', 'koko-analytics'); ?>
+                <?php esc_html_e('Switch to standalone view', 'koko-analytics'); ?>
             </a></li>
         <?php } ?>
     <?php } else { ?>
         <li><a href="<?php echo admin_url('index.php?page=koko-analytics'); ?>">
-            <?php echo esc_html__('Stats', 'koko-analytics'); ?>
+            <?php esc_html_e('Stats', 'koko-analytics'); ?>
         </a></li>
     <?php } ?>
 
     <li><a href="<?php echo admin_url('index.php?page=koko-analytics&tab=settings'); ?>" class="<?php echo $tab === 'settings' ? 'current' : ''; ?>">
-        <?php echo esc_html__('Settings', 'koko-analytics'); ?>
+        <?php esc_html_e('Settings', 'koko-analytics'); ?>
     </a></li>
 </nav>
 <?php } ?>
