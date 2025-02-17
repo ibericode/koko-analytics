248f96cfa62d3df783316bffa77d9858035d45ec
Danny van Kooten <hi@dvk.co>
2025-02-17 13:10:05 +0100

    Update external strings from Koko Analytics Pro

---
+++ src/external-strings.php
@@ -0,0 +1,64 @@
+<?php 
+
+// i18n strings for koko-analytics-pro
+
+// ../koko-analytics-pro/includes/views/dashboard-event-component.php#14
+__('Event:', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/dashboard-event-component.php#38
+__('There is nothing here. Yet!', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/dashboard-event-component.php#48
+__('Previous', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/dashboard-event-component.php#51
+__('Next', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/event-details.php#67
+__('Export to CSV', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/event-details.php#75
+__('Are you sure you want to delete this event?', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/event-details.php#77
+__('Delete event', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#10
+__('Events', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#17
+__('Track outbound link clicks?', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#26
+__('Track form submissions?', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#38
+__('Event types', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#39
+__('You currently have the following event types registered:', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#42
+__('Event name', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#53
+__('You have not yet registered any events.', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#59
+__('Use the form below to register new event types.', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#64
+__('Event name', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/views/events-section.php#67
+__('Add event', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/class-admin-bar.php#57
+__('Pageviews', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/class-posts-table.php#78
+__('Number of days to use for pageviews column', 'koko-analytics');
+
+// ../koko-analytics-pro/includes/class-posts-table.php#86
+__('Pageviews', 'koko-analytics');
+
