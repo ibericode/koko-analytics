679dae1ef1e7af5b0db5c9a9ae4b1fbb511fe2bb
Danny van Kooten <hi@dvk.co>
2025-02-24 13:58:30 +0100

    move use statement to first lines of file

--- migrations/1.6.7-protect-uploads-dir.php
+++ migrations/1.6.7-protect-uploads-dir.php
@@ -1,8 +1,8 @@
 <?php
 
-defined('ABSPATH') or exit;
-
 use KokoAnalytics\Plugin;
+
+defined('ABSPATH') or exit;
 
 if (class_exists(Plugin::class) && method_exists(Plugin::class, 'create_and_protect_uploads_dir')) {
     Plugin::create_and_protect_uploads_dir();
