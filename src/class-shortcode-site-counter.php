d03346a8f3acf4c04b7b5eae8827ed8588a61319
Danny van Kooten <hi@dvk.co>
2025-02-25 08:48:14 +0100

    increase transient timeout for \[koko_analytics_counter] shortcode to 5 minutes (from 1 minute)

--- src/class-shortcode-site-counter.php
+++ src/class-shortcode-site-counter.php
@@ -30,18 +30,18 @@ class Shortcode_Site_Counter
         $args = shortcode_atts($default_args, $args, self::SHORTCODE);
         $args['days'] = abs((int) $args['days']);
 
-        $id = $args['global'] && $args['global'] !== 'false' && $args['global'] !== '0' ? 0 : (int) get_the_ID();
+        $id = $args['global'] && $args['global'] !== 'false' && $args['global'] !== '0' && $args['global'] !== 'no' ? 0 : (int) get_the_ID();
         $start_date_str = $args['days'] === 0 ? 'today midnight' : "-{$args['days']} days";
         $start_date = create_local_datetime($start_date_str)->format('Y-m-d');
         $end_date = create_local_datetime('tomorrow midnight')->format('Y-m-d');
 
         $cache_key = 'ka_counter_' . $id . $args['metric'][0] . $args['days'];
         $count = get_transient($cache_key);
-        if (!$count) {
+        if (false === $count) {
             $stats = new Stats();
             $totals = $stats->get_totals($start_date, $end_date, $id);
             $count = $args['metric'] === 'pageviews' ? $totals->pageviews : $totals->visitors;
-            set_transient($cache_key, $count, 60);
+            set_transient($cache_key, $count, 5*60);
         }
 
         return '<span class="koko-analytics-counter">' . $count . '</span>';
