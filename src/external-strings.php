595ab67445566d75137f8fc8504e750a16341802
Danny van Kooten <hi@dvk.co>
2025-02-25 08:59:40 +0100

    Update external strings from Koko Analytics Pro

--- src/external-strings.php
+++ src/external-strings.php
@@ -59,8 +59,8 @@ __('Download as CSV', 'koko-analytics');
 // ../koko-analytics-pro/includes/class-admin-bar.php#57
 __('Pageviews', 'koko-analytics');
 
-// ../koko-analytics-pro/includes/class-posts-table.php#78
+// ../koko-analytics-pro/includes/class-posts-table.php#90
 __('Number of days to use for pageviews column', 'koko-analytics');
 
-// ../koko-analytics-pro/includes/class-posts-table.php#86
+// ../koko-analytics-pro/includes/class-posts-table.php#98
 __('Pageviews', 'koko-analytics');
