bed3dbc95a5cc32367ef2eb682cc35ac00ad0d37
Danny van Kooten <hi@dvk.co>
2025-02-26 08:51:16 +0100

    add failing test for consolidating www.reddit.com into reddit.com #291

--- tests/PageviewAggregatorTest.php
+++ tests/PageviewAggregatorTest.php
@@ -91,6 +91,7 @@ final class PageviewAggregatorTest extends TestCase
             'https://out.reddit.com/r/foobar' => 'https://reddit.com/r/foobar',
             'https://new.reddit.com/r/foobar' => 'https://reddit.com/r/foobar',
             'https://old.reddit.com/r/foobar' => 'https://reddit.com/r/foobar',
+            'https://www.reddit.com/r/foobar' => 'https://reddit.com/r/foobar',
             'https://6gg78.r.ah.d.sendibm4.com/mk/cl/f/sugrxasd218e287' => 'https://www.brevo.com',
             'https://6gg78.r.ah.d.sendibt1.com/mk/cl/f/sugrxasd218e287' => 'https://www.brevo.com',
         ];
@@ -104,8 +105,7 @@ final class PageviewAggregatorTest extends TestCase
     {
         $a = new \KokoAnalytics\Pageview_Aggregator();
 
-        foreach (
-            [
+        foreach ([
             'https://www.kokoanalytics.com',
             'android-app://com.google.android.googlequicksearchbox',
             ] as $url
@@ -113,8 +113,7 @@ final class PageviewAggregatorTest extends TestCase
             $this->assertTrue($a->is_valid_url($url));
         }
 
-        foreach (
-            [
+        foreach ([
              '',
              'Hello world',
              '<script>alert(1)</script>',
