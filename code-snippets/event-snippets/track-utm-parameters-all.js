a3c844596402fade1cd355bdc7ce33b36e491549
Danny van Kooten <hi@dvk.co>
2025-03-10 15:47:05 +0100

    make sure to send captured value

--- code-snippets/event-snippets/track-utm-parameters-all.js
+++ code-snippets/event-snippets/track-utm-parameters-all.js
@@ -1,7 +1,8 @@
 // This code snippets allows you to send URL parameters containing "utm_source" to an event named "UTM Source" in Koko Analytics
 
-var source = window.location.hash.match(/[#&]utm_source=([^&]*)/);
 
+
+var source = window.location.hash.match(/[#&]utm_source=([^&]*)/);
 if (source) {
   window.koko_analytics.trackEvent('UTM Source', source[1]);
 }
