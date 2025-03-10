8f927eaa555422541f56dba2a7a3b75a995941a9
Danny van Kooten <hi@dvk.co>
2025-03-10 16:01:32 +0100

    update UTM snippet to also look at search params

--- code-snippets/event-snippets/track-utm-parameters-all.js
+++ code-snippets/event-snippets/track-utm-parameters-all.js
@@ -1,8 +1,18 @@
-// This code snippets allows you to send URL parameters containing "utm_source" to an event named "UTM Source" in Koko Analytics
+// This code snippets monitors the URL for UTM parameters like "utm_source=WordPress.org" and sends it to a Koko Analytics custom event
+// In order for this to work, you need to have the custom event created in your Koko Analytics dashboard settings.
 
+let map = {
+  'utm_source': 'UTM Source',
+  'utm_medium': 'UTM Medium',
+  'utm_campaign': 'UTM Campaign',
+};
 
-
-var source = window.location.hash.match(/[#&]utm_source=([^&]*)/);
-if (source) {
-  window.koko_analytics.trackEvent('UTM Source', source[1]);
+let queryParams = new URLSearchParams(window.location.search);
+let hashParams = new URLSearchParams(window.location.hash.substring(1));
+for (let [p, eventName] of Object.entries(map)) {
+  let value = queryParams.get(p) ?? hashParams.get(p);
+  if (value) {
+    window.koko_analytics.trackEvent(eventName, value);
+  }
 }
+
--- code-snippets/event-snippets/track-utm-parameters.js
+++ code-snippets/event-snippets/track-utm-parameters.js
@@ -1,7 +1,12 @@
-// This code snippets allows you to send URL parameters containing "utm_source" to an event named "UTM Source" in Koko Analytics
+// This code snippets monitors the URL for UTM parameters like "utm_source=WordPress.org" and sends it to a Koko Analytics custom event
+// In order for this to work, you need to have the custom event created in your Koko Analytics dashboard settings.
 
-var source = window.location.hash.match(/[#&]utm_source=([^&]*)/);
+const PARAMETER = 'utm_source';
+const EVENT_NAME = 'UTM Source';
 
-if (source) {
-  window.koko_analytics.trackEvent('UTM Source', source[1]);
+let queryParams = new URLSearchParams(window.location.search);
+let hashParams = new URLSearchParams(window.location.hash.substring(1));
+let value = queryParams.get(PARAMETER) ?? hashParams.get(PARAMETER);
+if (value) {
+  window.koko_analytics.trackEvent(EVENT_NAME, value);
 }
