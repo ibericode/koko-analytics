4573b3aac5c894d8fc640344e29e3944ac2ac828
Danny van Kooten <hi@dvk.co>
2025-05-28 10:52:46 +0200

    add complianz snippet

---
+++ code-snippets/complianz-use-cookie-after-consent.js
@@ -0,0 +1,7 @@
+// To instruct Koko Analytics to use cookies only after consent has been given:
+// 1. Configure Koko Analytics to not use any cookies from the settings page.
+// 2. In your cookie banner plugin (eg Complianz), run the following script once consent is given:
+
+window.koko_analytics.use_cookie = true;
+
+// In Complianz, you can find this page at Complianz > Integrations > Script Center.
