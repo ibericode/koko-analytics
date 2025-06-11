04a74324e502ddf6f006f12a64b7fa058d2faccd
Danny van Kooten <hi@dvk.co>
2025-06-11 11:02:21 +0200

    back to sendBeacon

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -5,49 +5,30 @@
  */
 
 // Map variables to global identifiers so that minifier can mangle them to even shorter names
-var doc = document;
 var win = window;
-var nav = navigator;
 var ka = "koko_analytics";
 
-function request(params) {
-  if (!win[ka].urls.length) return;
-
-  var url = win[ka].urls[0];
-
+function request(data) {
   // if window.koko_analytics.use_cookie is set, use that (for cookie consent plugins)
-  var m = win[ka].use_cookie ? 'c' : win[ka].method[0];
+  data['m'] = win[ka].use_cookie ? 'c' : win[ka].method[0];
 
-  url += (url.indexOf('?') > -1 ? '&' : '?');
-  url += params;
-  url += "&m=";
-  url += m;
-
-  win.fetch(url, { method: 'POST', cache: 'no-store', priority: 'low' })
-    .then(function(response) {
-      // verify response: if failed, try next url from config array
-      if ((!response.ok || response.headers.get('Content-Type').indexOf('text/plain') === -1)) {
-        win[ka].urls.shift();
-        request(params + "&disable-custom-endpoint=1");
-      }
-    });
+  navigator.sendBeacon(win[ka].url, new URLSearchParams(data));
 }
 
 win[ka].trackPageview = function(postId) {
   if (
     // do not track if this is a prerender request
-    (doc.visibilityState == 'prerender') ||
+    (document.visibilityState == 'prerender') ||
 
     // do not track if user agent looks like a bot
-    ((/bot|crawl|spider|seo|lighthouse|facebookexternalhit|preview/i).test(nav.userAgent))
+    ((/bot|crawl|spider|seo|lighthouse|facebookexternalhit|preview/i).test(navigator.userAgent))
   ) {
     return;
   }
 
   // don't store referrer if from same-site
-  var referrer = doc.referrer.indexOf(win[ka].site_url) == 0 ? '' : doc.referrer;
-
-  request("p="+postId+"&r="+encodeURIComponent(referrer));
+  var referrer = document.referrer.indexOf(win[ka].site_url) == 0 ? '' : document.referrer;
+  request({ p: postId, r: referrer })
 }
 
 win.addEventListener('load', function() {
