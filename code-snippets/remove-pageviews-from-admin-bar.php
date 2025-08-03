2fccd07b9c2214b0fb29be24606678a0c8e315d6
Danny van Kooten <hi@dvk.co>
2025-08-03 14:55:53 +0200

    fix code style issues

--- code-snippets/remove-pageviews-from-admin-bar.php
+++ code-snippets/remove-pageviews-from-admin-bar.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * Plugin Name: Koko Analytics: Remove pageviews component from Admin Bar
  */
+
 add_action('plugins_loaded', function () {
     remove_action('admin_bar_menu', [\KokoAnalytics\Pro\Admin_Bar::class, 'register'], 120);
 });
