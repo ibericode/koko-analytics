f522275708faefe3f4e1fd8f71c01c18b87a794e
Danny van Kooten <hi@dvk.co>
2025-08-21 09:59:29 +0200

    improve referrer migration by consolidating duplicate entries

--- migrations/1.9.992-normalize-referrer-urls.php
+++ migrations/1.9.992-normalize-referrer-urls.php
@@ -12,5 +12,14 @@ global $wpdb;
 $results = $wpdb->get_results("SELECT id, url FROM {$wpdb->prefix}koko_analytics_referrer_urls WHERE url LIKE 'http%'");
 foreach ($results as $row) {
     $row->url = Normalizer::referrer($row->url);
-    $wpdb->query($wpdb->prepare("UPDATE {$wpdb->prefix}koko_analytics_referrer_urls SET url = %s WHERE id = %s LIMIT 1", [ $row->url, $row->id ]));
+
+    // check if normalized url already has an entry
+    $id = $wpdb->get_var($wpdb->prepare("SELECT id FROM {$wpdb->prefix}koko_analytics_referrer_urls WHERE url = %s", [$row->url]));
+    if ($id) {
+        // if so, update stats to point to nexisting entry
+        $wpdb->query($wpdb->prepare("UPDATE {$wpdb->prefix}koko_analytics_referrer_stats SET id = %d WHERE id = %d", [ $id, $row->id ]));
+    } else {
+        // otherwise change entry to normalized version
+        $wpdb->query($wpdb->prepare("UPDATE {$wpdb->prefix}koko_analytics_referrer_urls SET url = %s WHERE id = %s LIMIT 1", [ $row->url, $row->id ]));
+    }
 }
