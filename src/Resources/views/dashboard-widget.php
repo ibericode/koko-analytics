3bf124d762e359dd1090a44a8174df68e2910113
Danny van Kooten <hi@dvk.co>
2025-08-26 13:29:10 +0200

    dashboard widget should mostly match wp dashboard styles

--- src/Resources/views/dashboard-widget.php
+++ src/Resources/views/dashboard-widget.php
@@ -18,19 +18,21 @@ use KokoAnalytics\Fmt;
 <link rel="stylesheet" href="<?php echo plugins_url('assets/dist/css/dashboard-2.css', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?php echo KOKO_ANALYTICS_VERSION; ?>">
 <script src="<?php echo plugins_url('assets/dist/js/dashboard-widget.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?php echo KOKO_ANALYTICS_VERSION; ?>" defer></script>
 
-<div class="koko-analytics" style="background: inherit;">
+<div >
     <div id="ka-dashboard-widget-realtime" class="mb-4">
-        <h3 class="fs-6 mb-2 fw-bold"><?php esc_html_e('Realtime', 'koko-analytics'); ?></h3>
+        <h3 class="mb-2"><?php esc_html_e('Realtime', 'koko-analytics'); ?></h3>
         <p class="m-0">
             <?php printf(esc_html__('Your site had a total of %1$s pageviews today, %2$s of which were in the last hour.', 'koko-analytics'), '<strong>' . number_format_i18n($totals->pageviews) . '</strong>', '<strong>' . number_format_i18n($realtime) . '</strong>'); ?>
         </p>
     </div>
 
     <div id="ka-dashboard-widget-chart" class="mb-4">
-        <h3 class="fs-6 mb-2 fw-bold">
+        <h3 class="mb-3">
            <?php esc_html_e('Showing site visits over last 14 days', 'koko-analytics'); ?>
         </h3>
+        <div class="">
         <?php new Chart_View($chart_data, $dateStart, $dateEnd, 200, false); ?>
+        </div>
     </div>
 
     <?php if ($number_of_top_items > 0 && (count($posts) > 0 || count($referrers) > 0)) { ?>
@@ -38,7 +40,7 @@ use KokoAnalytics\Fmt;
         <div class="ka-row ka-row-cols-2">
             <?php if (count($posts) > 0) { ?>
             <div class="ka-col">
-                <h3 class="fs-6 mb-2 fw-bold">
+                <h3 class="mb-2">
                     <?php esc_html_e('Today\'s top pages', 'koko-analytics'); ?>
                 </h3>
                 <ul class="list-unstyled m-0">
@@ -53,7 +55,7 @@ use KokoAnalytics\Fmt;
             <?php } // end if count posts ?>
             <?php if (count($referrers) > 0) { ?>
             <div class="ka-col">
-                <h3 class="fs-6 mb-2 fw-bold">
+                <h3 class="mb-2">
                 <?php esc_html_e('Today\'s top referrers', 'koko-analytics'); ?>
                 </h3>
                 <ul class="list-unstyled m-0">
