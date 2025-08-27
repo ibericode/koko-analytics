9d5433778af181461d811bedec843b46a345835c
Danny van Kooten <hi@dvk.co>
2025-08-27 13:38:01 +0200

    split migrations into multiple files

--- migrations/1.9.991-prepare-post-stats-table.php
+++ migrations/1.9.991-prepare-post-stats-table.php
@@ -2,8 +2,6 @@
 
 defined('ABSPATH') or exit;
 
-@set_time_limit(0);
-
 /** @var wpdb $wpdb */
 global $wpdb;
 
@@ -14,11 +12,6 @@ $wpdb->query(
        INDEX (path(191))
     ) ENGINE=INNODB CHARACTER SET=utf8mb4"
 );
-
-// create back-up of post_stats table
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}koko_analytics_post_stats_backup");
-$wpdb->query("CREATE TABLE {$wpdb->prefix}koko_analytics_post_stats_backup LIKE {$wpdb->prefix}koko_analytics_post_stats");
-$wpdb->query("INSERT INTO {$wpdb->prefix}koko_analytics_post_stats_backup SELECT * FROM {$wpdb->prefix}koko_analytics_post_stats");
 
 // prepare columns
 $wpdb->query("ALTER TABLE {$wpdb->prefix}koko_analytics_post_stats CHANGE COLUMN id post_id MEDIUMINT UNSIGNED");
