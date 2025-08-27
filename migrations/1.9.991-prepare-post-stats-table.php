ea14257e498166c178afdf2313f9a94a284d752d
Danny van Kooten <hi@dvk.co>
2025-08-27 14:33:19 +0200

    add proper primary key whenever post_stats table is ready

--- migrations/1.9.991-prepare-post-stats-table.php
+++ migrations/1.9.991-prepare-post-stats-table.php
@@ -16,3 +16,12 @@ $wpdb->query(
 // prepare columns
 $wpdb->query("ALTER TABLE {$wpdb->prefix}koko_analytics_post_stats CHANGE COLUMN id post_id MEDIUMINT UNSIGNED");
 $wpdb->query("ALTER TABLE {$wpdb->prefix}koko_analytics_post_stats ADD COLUMN path_id MEDIUMINT UNSIGNED");
+
+$results = $wpdb->get_var("SELECT COUNT(DISTINCT(post_id)) FROM {$wpdb->prefix}koko_analytics_post_stats WHERE post_id IS NOT NULL AND path_id IS NULL");
+if (!$results) {
+   // make new path_id column not-nullable
+    $wpdb->query("ALTER TABLE {$wpdb->prefix}koko_analytics_post_stats MODIFY COLUMN path_id MEDIUMINT UNSIGNED NOT NULL");
+
+  // change primary key to be on date and path_id column
+    $wpdb->query("ALTER TABLE {$wpdb->prefix}koko_analytics_post_stats DROP PRIMARY KEY, ADD PRIMARY KEY(date, path_id)");
+}
