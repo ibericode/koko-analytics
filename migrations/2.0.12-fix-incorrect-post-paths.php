cf8ed529309c115f55a165ff6df05b28957ffe44
Danny van Kooten <hi@dvk.co>
2025-09-02 09:26:51 +0200

    auto-fix post paths on small sites

---
+++ migrations/2.0.12-fix-incorrect-post-paths.php
@@ -0,0 +1,13 @@
+<?php
+
+use KokoAnalytics\Admin\Actions;
+
+defined('ABSPATH') or exit;
+
+/** @var \wpdb $wpdb */
+global $wpdb;
+
+$count = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}koko_analytics_post_stats");
+if ($count && $count < 25000 && method_exists(Actions::class, 'fix_post_paths_after_v2')) {
+    Actions::fix_post_paths_after_v2();
+}
