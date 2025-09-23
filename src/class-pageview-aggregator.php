3695dcee7f679d8261f68743ca9e537c535bd311
Danny van Kooten <hi@dvk.co>
2025-09-23 12:48:16 +0200

    add filename alias for Pageview_Aggregator class so old autoloader knows where to find it. #319

---
+++ src/class-pageview-aggregator.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file is loaded from the 1.8.x autoloader and that's why we keep it around.
+ * This helps smoothens the upgrade to 2.x by at least preventing the error directly after the update (using the 1.8.x autoloader which is in memory at that point)
+ */
+
+require __DIR__ . '/Pageview_Aggregator.php';
