3bafb0455df85fd4afc616dfcb4dbaafe7e0af90
Danny van Kooten <hi@dvk.co>
2025-10-03 15:09:19 +0200

    add whitespace to meta robots directive

--- src/Resources/views/standalone.php
+++ src/Resources/views/standalone.php
@@ -10,7 +10,6 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
     <script src="<?php echo plugins_url('assets/dist/js/dashboard.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?php echo KOKO_ANALYTICS_VERSION; ?>" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="referrer" content="no-referrer-when-downgrade">
-    <meta name="robots" content="noindex,nofollow">
     <title>Koko Analytics</title>
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-title" content="Koko Analytics">
@@ -18,8 +17,9 @@ defined('ABSPATH') or exit; ?><!DOCTYPE html>
     <link rel="apple-touch-icon" href="<?php echo plugins_url('assets/dist/img/apple-touch-icon.png', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
     <link rel="manifest" href="<?php echo plugins_url('assets/dist/manifest.json', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
     <link rel="shortcut icon" href="<?php echo plugins_url('assets/dist/img/favicon.ico', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
-    <meta name="theme-color" content="#B60205">
     <link rel="canonical" href="<?= site_url('?koko-analytics-dashboard'); ?>">
+    <meta name="robots" content="nofollow, noindex">
+    <meta name="theme-color" content="#B60205">
 </head>
 <body class="koko-analytics">
     <?php $this->show(); ?>
