09d706d07c5bf392337385047a08e2e96ac327dd
Danny van Kooten <hi@dvk.co>
2025-11-05 12:57:02 +0100

    modify snippet for adding capability to editor role

--- code-snippets/allow-editor-role.php
+++ code-snippets/allow-editor-role.php
@@ -1,12 +1,9 @@
 <?php
 
 add_action('admin_init', function () {
-    // only run on koko analytics page
-    if (! isset($_GET['page']) || $_GET['page'] !== 'koko-analytics') {
-        return;
-    }
-
     // add "view_koko_analytics" capability to "editor" role
     $role = get_role('editor');
-    $role->add_cap('view_koko_analytics');
+    if (!$role->has_cap('view_koko_analytics')) {
+        $role->add_cap('view_koko_analytics');
+    }
 });
