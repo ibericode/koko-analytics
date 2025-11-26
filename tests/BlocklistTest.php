01fa7051257b9e702b8a095e60aa10a6fd8fdbd4
Danny van Kooten <hi@dvk.co>
2025-11-26 09:49:06 +0100

    run test on potentially non-existing blocklist to ensure no exceptions thrown

--- tests/BlocklistTest.php
+++ tests/BlocklistTest.php
@@ -13,6 +13,7 @@ final class BlocklistTest extends TestCase
     public function test(): void
     {
         $b = new Blocklist();
+        self::assertIsBool($b->contains(''));
         $b->update(true);
         self::assertTrue($b->contains('1xslot.site'));
         self::assertFalse($b->contains('kokoanalytics.com'));
