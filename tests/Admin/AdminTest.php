5d3371d43f461ae8d6c05e8cfa9b98ae72525560
Danny van Kooten <hi@dvk.co>
2026-02-02 15:59:22 +0100

    [tests] fix classname: Admin -> Controller

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace KokoAnalytics\Tests\Admin;
 
-use KokoAnalytics\Admin\Admin;
+use KokoAnalytics\Admin\Controller;
 use PHPUnit\Framework\TestCase;
 
 final class AdminTest extends TestCase
 {
     public function testCanInstantiate(): void
     {
-        $i = new Admin();
-        self::assertTrue($i instanceof Admin);
+        $i = new Controller();
+        self::assertTrue($i instanceof Controller);
     }
 }
