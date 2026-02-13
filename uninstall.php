2d994531eabc84c052b71aab8184ccf9fb38f50e
Danny van Kooten <hi@dvk.co>
2026-02-13 12:18:59 +0100

    [chore] delete koko_analytics_last_aggregation_at on uninstall, document options explicitly not deleted

--- uninstall.php
+++ uninstall.php
@@ -18,3 +18,7 @@ delete_option("koko_analytics_settings");
 delete_option("koko_analytics_use_custom_endpoint");
 delete_option("koko_analytics_realtime_pageview_count");
 delete_option('koko_analytics_jetpack_import_params');
+delete_option('koko_analytics_last_aggregation_at');
+
+// not removing koko_analytics_version because the database tables itself are not removed upon uninstall
+// not removing koko_analytics_referrers_v2 because it also describes database state
