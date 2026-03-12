76dde14056b31f00020bcd65607ab3fff5b71b5c
Danny van Kooten <hi@dvk.co>
2026-03-12 13:40:18 +0100

    nit: add html comment about plugin to public dashboard source

--- src/Resources/views/dashboard-public.php
+++ src/Resources/views/dashboard-public.php
@@ -3,21 +3,22 @@
  * @var KokoAnalytics\Dashboard $this
  */
 defined('ABSPATH') or exit; ?>
+<!-- This dashboard is powered by Koko Analytics: privacy-friendly website analytics for WordPress. Find out more at https://kokoanalytics.com/ -->
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language'); ?>">
 <head>
     <meta name="charset" content="<?php bloginfo('charset'); ?>">
-    <link rel="stylesheet" href="<?php echo plugins_url('assets/dist/css/dashboard-2.css', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?php echo KOKO_ANALYTICS_VERSION; ?>">
-    <script src="<?php echo plugins_url('assets/dist/js/dashboard.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?php echo KOKO_ANALYTICS_VERSION; ?>" defer></script>
+    <link rel="stylesheet" href="<?= plugins_url('assets/dist/css/dashboard-2.css', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?= KOKO_ANALYTICS_VERSION; ?>">
+    <script src="<?= plugins_url('assets/dist/js/dashboard.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>?v=<?= KOKO_ANALYTICS_VERSION; ?>" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="referrer" content="no-referrer-when-downgrade">
     <title>Koko Analytics</title>
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-title" content="Koko Analytics">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <link rel="apple-touch-icon" href="<?php echo plugins_url('assets/dist/img/apple-touch-icon.png', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
-    <link rel="manifest" href="<?php echo plugins_url('assets/dist/manifest.json', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
-    <link rel="shortcut icon" href="<?php echo plugins_url('assets/dist/img/favicon.ico', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
+    <link rel="apple-touch-icon" href="<?= plugins_url('assets/dist/img/apple-touch-icon.png', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
+    <link rel="manifest" href="<?= plugins_url('assets/dist/manifest.json', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
+    <link rel="shortcut icon" href="<?= plugins_url('assets/dist/img/favicon.ico', KOKO_ANALYTICS_PLUGIN_FILE); ?>">
     <link rel="canonical" href="<?= site_url('/koko-analytics-dashboard/'); ?>">
     <meta name="robots" content="nofollow, noindex">
     <meta name="theme-color" content="#B60205">
@@ -27,7 +28,7 @@ defined('ABSPATH') or exit; ?>
     <script>
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register(
-                '<?php echo plugins_url('assets/dist/js/sw.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>'
+                '<?= plugins_url('assets/dist/js/sw.js', KOKO_ANALYTICS_PLUGIN_FILE); ?>'
             );
         }
     </script>
