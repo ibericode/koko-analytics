51678817a9b97082f99befbf287015995bbf5c09
Danny van Kooten <hi@dvk.co>
2026-04-06 14:55:32 +0200

    i18n: run koko_analytics_counter shortcode content through number_format_i18n

--- src/Shortcodes/Shortcode_Site_Counter.php
+++ src/Shortcodes/Shortcode_Site_Counter.php
@@ -35,21 +35,20 @@ class Shortcode_Site_Counter
         $args['days'] = abs((int) $args['days']);
         $path = $args['global'] !== false && $args['global'] !== 'false' && $args['global'] !== '0' && $args['global'] !== 'no' ? '' : $this->get_post_path();
 
-        $start_date_str = $args['days'] === 0 ? 'today midnight' : "-{$args['days']} days";
-        $timezone = wp_timezone();
-        $start_date = (new DateTime($start_date_str, $timezone))->format('Y-m-d');
-        $end_date = (new DateTime('tomorrow, midnight', $timezone))->format('Y-m-d');
-
         $cache_key = "ka_counter_" . md5("{$path}-{$args['metric']}-{$args['days']}");
         $count = get_transient($cache_key);
         if (false === $count) {
             $stats = new Stats();
+            $start_date_str = $args['days'] === 0 ? 'today midnight' : "-{$args['days']} days";
+            $timezone = wp_timezone();
+            $start_date = (new DateTime($start_date_str, $timezone))->format('Y-m-d');
+            $end_date = (new DateTime('tomorrow, midnight', $timezone))->format('Y-m-d');
             $totals = $stats->get_totals($start_date, $end_date, $path);
             $count = $args['metric'] === 'visitors' ? $totals->visitors : $totals->pageviews;
             set_transient($cache_key, $count, 5 * 60);
         }
 
-        return '<span class="koko-analytics-counter">' . $count . '</span>';
+        return '<span class="koko-analytics-counter">' . number_format_i18n($count) . '</span>';
     }
 
     // Gets the path to whatever post is currently in "the loop"
