3545d2df1e54337374de08fd19a2f2ff58d869fc
Danny van Kooten <hi@dvk.co>
2026-04-25 13:07:59 +0200

    update sample event snippet to not rely on jquery

--- code-snippets/pro/event-snippets/track-mailto-or-tel-link-clicks.js
+++ code-snippets/pro/event-snippets/track-mailto-or-tel-link-clicks.js
@@ -1,14 +1,16 @@
 // If mail or tel links clicked
-jQuery('body').on('click', 'a', function(event) {
-    var href = jQuery(this).attr('href');
 
+document.addEventListener('click', function(evt) {
+    if (!evt.target || evt.target.tagName !== 'A' || ! evt.target.getAttribute('href')) {
+        return;
+    }
+
+    let href = evt.target.getAttribute('href');
     if (href.startsWith("tel:")) {
-        var phoneNumber = href.replace("tel:", "");
-        window.koko_analytics.trackEvent('Phone Clicked', 'From page: ' +
-        window.location.pathname + ' Ph:' + phoneNumber);
+        let phoneNumber = href.replace("tel:", "");
+        window.koko_analytics.trackEvent('Phone Clicked', 'From page: ' + window.location.pathname + ' Ph:' + phoneNumber);
     } else if (href.startsWith("mailto:")) {
-        var emailAddress = href.replace("mailto:", "");
-        window.koko_analytics.trackEvent('Email Clicked',  'From page: ' +
-        window.location.pathname + ' Email:' + emailAddress);
+        let emailAddress = href.replace("mailto:", "");
+        window.koko_analytics.trackEvent('Email Clicked',  'From page: ' + window.location.pathname + ' Email:' + emailAddress);
     }
 });
