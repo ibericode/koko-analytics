f1889ef32866fb001c742bbb7da021b8d68011f7
Danny van Kooten <hi@dvk.co>
2026-06-08 15:17:23 +0200

    docs: add sample snippet for setting item limits in koko analytics pro email reports

---
+++ code-snippets/pro/change-email-report-item-limits.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Koko Analytics Pro: Change Email Report Item Limits
+ *
+ * Set a limit to 0 to hide that section from periodic email reports.
+ */
+
+add_filter('koko_analytics_email_report_item_limits', function ($limits) {
+    $limits['posts'] = 15;
+    $limits['referrers'] = 15;
+    $limits['countries'] = 10;
+    $limits['browsers'] = 0;
+    $limits['operating_systems'] = 0;
+    $limits['devices'] = 5;
+
+    return $limits;
+});
